996dfb2b57a2aa4a57d6c1c00f4ea5a3f945f56d
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-23 15:15:48 +0530

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  # root 'users#index'
+  # get 'users/745', to: 'users#show'
+  # get 'users/745/posts', to: 'posts#index'
+  # get 'users/745/posts/3', to: 'posts#show'
 
-  get 'users/745', to: 'users#show'
-
-  get 'users/745/posts', to: 'posts#index'
-
-  get 'users/745/posts/3', to: 'posts#show'
+  resources :users, only: [:show, :index] do
+    resources :posts, only: [:show, :index]
+   end
 end
