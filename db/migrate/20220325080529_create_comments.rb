7d1761f6fe76f3db36d6572685037678c8464ada
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-25 13:52:02 +0530

    Add migration for post table

--- db/migrate/20220325080529_create_comments.rb
+++ db/migrate/20220325080529_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :Comment do |t|
       t.references :User, null: false, foreign_key: true, index: true
-      t.references :Post null: false, foreign_key: true, index: true
+      t.references :Post, null: false, foreign_key: true, index: true
       t.text :Text
       t.timestamps
     end
