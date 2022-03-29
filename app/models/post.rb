63bc8f374cf3bd59da15921edda91c710b8254b7
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-29 13:48:50 +0530

    Add recent comments method in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,8 @@ class Post < ApplicationRecord
   def update_posts_counter
     user.increment!(: posts_counter)
   end
+
+  def recent_comments
+    comments.limit(5).order(created_at: desc);
+  end
 end
