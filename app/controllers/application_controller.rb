c0391e67b5e1b7f8b631ae4e9fecb685502e081c
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-29 13:53:08 +0530

    Add current_user to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  def current_user
+    User.limit(1)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,7 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
   
+  def update_comments_counter
+    post.increment!(: like_counter)
+  end
 end
