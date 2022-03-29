86fdae0bda6a066f4e44dca68582bbfba0c8bf11
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-29 13:44:45 +0530

    Add recent post method in user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
+  has_many :posts
+  has_many :comments
+  has_many :likes
+
+  def recent_posts
+    posts.limit(3).order(created_at: :desc)
+  end
 
 end
