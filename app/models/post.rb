9b5218180f3a9d877e9ffcff46744495c527f89f
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-03-31 13:44:27 +0530

    Update models

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,15 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
-
-  def update_posts_counter
-    author.increment!(:posts_counter)
-  end
+  after_save :update_posts_counter
 
   def recent_comments
     comments.limit(5).order(created_at: :desc)
+  end
+
+  private 
+  
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 end
