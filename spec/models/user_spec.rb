ce2df655e6664753df4b7f9c201ed4b479b23088
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-01 16:22:12 +0530

    Update tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,15 +20,7 @@ RSpec.describe User, type: :model do
     it 'check if posts counter is equal or greater than zero' do
       expect(subject.posts_counter).to be >= 0
     end
-  end
-
-  describe 'Validate Recent posts method' do
-    before do
-      4.times do |post|
-        Post.create(author_id: subject, title: "It is a post #{post}", text: 'This is text')
-      end
-    end
-
+    
     it 'shows three recent posts' do
       expect(subject.recent_posts).to eq(subject.posts.last(3))
     end
