439774c8fdd9f6d41ad98032d7d9de4ef0e37599
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-06 15:56:10 +0530

    Add test for posts_index

---
+++ spec/views/posts/posts_index_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+
+RSpec.describe 'posts#index', type: :feature do
+  describe 'User' do
+    before(:each) do
+      @user1 = User.create(name: 'Amy', photo: 'Tom.png', bio: 'bio', posts_counter: 0, email: 'amy@gmail.com',
+                           password: 'password')
+      visit root_path
+      fill_in 'Email', with: 'amy@gmail.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Log in'
+
+      @post1 = Post.create(title: 'First Post', text: 'This is my first post', comments_counter: 0, like_counter: 0,
+                           author: @user1)
+      @post2 = Post.create(title: 'Second Post', text: 'This is my second post', comments_counter: 0, like_counter: 0,
+      author: @user1)
+      @post3 = Post.create(title: 'Third Post', text: 'This is my third post', comments_counter: 0, like_counter: 0,
+      author: @user1)
+      @post4 = Post.create(title: 'Fourth Post', text: 'This is my fourth post', comments_counter: 0, like_counter: 0,
+      author: @user1)
+      @comment1 = Comment.create(text: 'Good job!', author: User.first,
+                                 post: Post.first)
+      @comment2 = Comment.create(text: 'Keep it up!', author: User.first, post: Post.first)
+      @comment3 = Comment.create(text: 'Congratulations!', author: User.first, post: Post.first)
+
+      visit(user_posts_path(@user1.id))
+    end
+
+    it "shows user's profile picture" do
+      all('img').each do |i|
+        expect(i[:src]).to eq('Tom.png')
+      end
+    end
+
+    it 'shows the users username' do
+      expect(page).to have_content('Amy')
+    end
+
+    it 'shows number of posts of user has written' do
+      post = Post.all
+      expect(post.size).to eql(4)
+    end
+
+    it 'shows number of posts by user' do
+      user = User.first
+      expect(page).to have_content(user.posts_counter)
+    end
+
+    it 'shows posts title' do
+      expect(page).to have_content('First Post')
+      visit user_session_path
+    end
+
+    it 'can see some of the post detail' do
+      expect(page).to have_content 'This is my first post'
+    end
+
+    it 'can see the first comment on a post' do
+      expect(page).to have_content 'Good job!'
+    end
+
+    it 'can see how many comments a post has.' do
+      post = Post.first
+      expect(page).to have_content(post.comments_counter)
+    end
+
+    it 'can see how many likes a post has.' do
+      post = Post.first
+      expect(page).to have_content(post.like_counter)
+    end
+
+    it "redirects the user to the post's show page after clickin on it" do
+      click_link 'First Post'
+      expect(page).to have_current_path user_post_path(@post1.author_id, @post1)
+    end
+  end
+end
