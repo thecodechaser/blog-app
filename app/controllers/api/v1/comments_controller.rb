8658111386db6595570a5be1cf05ea810de65408
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-07 15:42:16 +0530

    Add api methods for comments

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,36 @@
+module Api
+  module V1
+    class CommentsController < ApplicationController
+      def index
+        if request.headers['X-AUTH-TOKEN']
+          @user = User.find_by_api_token(request.headers['X-AUTH-TOKEN'])
+          @comments = Comment.where(author_id: @user.id)
+          respond_to do |format|
+            format.json { render json: @comments, status: :ok }
+          end
+        else
+          respond_to do |format|
+            format.json { render json: 'please sign in or add the token'.to_json, status: :ok }
+          end
+        end
+      end
+
+      def create
+        if request.headers['X-AUTH-TOKEN']
+          @user = User.find_by_api_token(request.headers['X-AUTH-TOKEN'])
+          new_comment = @user.comments.new(text: params[:text])
+          new_comment.post_id = params[:post_id].to_i
+          if new_comment.save
+            render json: { success: true, message: 'Loaded comments', data: { comment: @comment } }, status: :created
+          else
+            render json: { success: false, errors: @comment.errors }, status: :unprocessable_entity
+          end
+        else
+          respond_to do |format|
+            format.json { render json: 'please sign in or add the token', status: :ok }
+          end
+        end
+      end
+    end
+  end
+end
