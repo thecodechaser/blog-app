54e193ac57ae7f0e86551572dcac829b462aa184
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-09 14:14:37 +0530

    Add API to create a post

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       post 'users/sign_up' => 'users#register'
       post 'users/sign_in' => 'users#login'
-      get 'posts/:token' => 'posts#index'
+      get 'posts' => 'posts#index'
+      post 'posts/create' => 'posts#create'
       get 'comments' => 'comments#index'
       post 'comments/create' => 'comments#create'
     end
