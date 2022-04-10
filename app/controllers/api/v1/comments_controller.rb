95ad0dceb5c53d349d89e5837d1336911042bbc5
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-10 12:43:28 +0530

    Update APIs

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,8 +6,12 @@ module Api
       def index
         if request.headers['X-AUTH-TOKEN']
           @user = User.find_by_api_token(request.headers['X-AUTH-TOKEN'])
+          if @user
           @comments = Comment.where(author_id: @user.id)
           render json: { success: true, message: 'Loaded all comments', data: { comments: @comments } }, status: :ok
+          else 
+            render json: { success: false, errors: 'Wrong authentication token' }, status: :unprocessable_entity
+          end
         else
           respond_to do |format|
             format.json { render json: 'please sign in or add the token'.to_json, status: :ok }
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -6,8 +6,12 @@ module Api
       def index
         if request.headers['X-AUTH-TOKEN']
           @user = User.find_by_api_token(request.headers['X-AUTH-TOKEN'])
+          if @user
           @posts = Post.where(author_id: @user.id)
           render json: { success: true, message: 'Loaded all posts', data: { posts: @posts } }, status: :ok
+        else 
+          render json: { success: false, errors: 'Wrong authentication token' }, status: :unprocessable_entity
+        end
         else
           respond_to do |format|
             format.json { render json: 'please sign in or add the token', status: :ok }
